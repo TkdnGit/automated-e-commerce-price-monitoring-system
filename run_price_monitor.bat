d8e2e679753783034bf725e56c474bb869f49140
Tapu Kumar Debnath <srdl.doict.9244@gmail.com>
2026-09-22 14:48:12 +0600

    Initial project upload

---
+++ run_price_monitor.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd /d "C:\Users\DoICT\OneDrive\Desktop\task_schdular"
+
+echo ==========================================
+echo FLIPKART PRICE MONITORING STARTED
+echo ==========================================
+
+"C:\Users\DoICT\AppData\Local\Programs\Python\Python313\python.exe" flipkart_price_monitor.py
+
+echo.
+echo ==========================================
+echo FLIPKART PRICE MONITORING COMPLETED
+echo ==========================================
+
+pause
